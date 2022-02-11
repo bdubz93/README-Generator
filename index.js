3330609d7b8c430e559d8121223061721c250863
Brandon Winkler <brandonwinkler@live.ca>
2022-02-10 19:39:00 -0500

    fixed an issue in index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function init() {
     inquirer
       .prompt(questions)
       .then(answers => {
-        writeToFile('README-sample.md',answers);
+        writeToFile('README.md',answers);
       })
       .catch(error => {
         console.log('Error');
